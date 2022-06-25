8c8736365e058dc325b68b811319bf34fbbea2dd
Akesh_Ramasinghe <akeshramasinghe@gmail.com>
2022-06-25 14:19:42 +0530

    build post method
    in post method no need to parse parameters from the route by default it can detect what are the parameters include in header.

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI # import packages
 
 app = FastAPI()
-
+list_username = list()
 @app.get("/home") # calliing route
 def write_home():
     return {
         "Name": "ABCD",
         "Age": 24
+    }
+@app.post("/username")
+def post_data(username : str):
+    list_username.append(username)
+    return {
+        "username": list_username
     }
