100faa41c2661b54a9786a634697f9e0b94d577b
Akesh_Ramasinghe <akeshramasinghe@gmail.com>
2022-06-25 20:58:26 +0530

    again add list

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ from fastapi import FastAPI # import packages
 
 app = FastAPI()
 list_username = list()
-username = "default"
+a = 10
 @app.get("/home") # calliing route
 def write_home():
     return {
-         "username": username
+         "username": list_username
     }
 @app.post("/username")
 def post_data(username : str):
-    #list_username.append(username)
+    list_username.append(username)
+    #a = 20
     return {
         "username": username
     }
