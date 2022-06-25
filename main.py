30e78ddfa31a32c42980a0a5046e1c6154e37356
Akesh_Ramasinghe <akeshramasinghe@gmail.com>
2022-06-25 20:47:41 +0530

    change the store variable

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ from fastapi import FastAPI # import packages
 
 app = FastAPI()
 list_username = list()
+username = "default"
 @app.get("/home") # calliing route
 def write_home():
     return {
-         "username": list_username
+         "username": username
     }
 @app.post("/username")
 def post_data(username : str):
-    list_username.append(username)
+    #list_username.append(username)
     return {
-        "username": list_username
+        "username": username
     }
