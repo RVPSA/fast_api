3f7da809ee1e99b1f69905e50f4d74b2df541e38
Akesh_Ramasinghe <akeshramasinghe@gmail.com>
2022-06-26 12:04:50 +0530

    update main.py
    create a list 'database which stores the details '

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI  # import packages
 from mode import User
+from typing import List
 
 app = FastAPI()
 list_username = list()
 a = 10
+
+dataBase : List[User] = [
+    User(
+        username = "User 1"
+    )
+]
+
 @app.get("/home") # calliing route
 def write_home():
     return {
